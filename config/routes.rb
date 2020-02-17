a7c7850e5ccb9af42124ab8aad1c840083b7c98e
Roxane Haddad <roxane.haddad@gmail.com>
2020-02-17 17:39:40 +0100

    changed and validated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
-    resources :kitches, only: [:new, :create, :edit, :update]
+    resources :kitchens, only: [:new, :create]
   end
-  resources :kitchens, only: [:destroy]
+  resources :kitchens, only: [:edit, :update, :destroy]
   resources :kitchens, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
