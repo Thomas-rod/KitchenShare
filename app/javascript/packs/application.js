bd47cb92ea887683ee1c1683cf674b537a6acefb
Jonathan Courdavault <jo.courdavault@gmail.com>
2020-02-17 18:27:01 +0100

    work in progress navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,13 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("turbolinks").start()
 
 import "bootstrap";
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
