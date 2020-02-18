066a51b430457058b240d277f5f6d75f9ab7153b
Maodo Diop <maodod1@gmail.com>
2020-02-18 18:21:56 +0100

    view new booking

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,5 +1,6 @@
 class Kitchen < ApplicationRecord
+  CAPACITY = (1..5)
   belongs_to :user
   has_many_attached :photos
-  # validates :city, :address, :capacity, :name, :equipment, :price_by_hour, presence: true
+  validates :city, :address, :capacity, :name, :equipment, :price_by_hour, presence: true
 end
