4d471c7eaf37ba130a5d68bbc19e9f5d23ea28a6
Roxane Haddad <roxane.haddad@gmail.com>
2020-02-19 11:03:45 +0100

    geocoded adresses

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,5 +1,7 @@
 class Kitchen < ApplicationRecord
   CAPACITY = (1..5)
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   has_many_attached :photos
   validates :city, :address, :capacity, :name, :equipment, :price_by_hour, presence: true
