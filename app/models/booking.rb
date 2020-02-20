e47c67ad309f0b33d09d315a9c3bef2f366f6c02
thomas rodier <rodiert17@gmail.com>
2020-02-20 23:39:33 +0100

    update bookings model/migration and set some changes in dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,9 @@
 class Booking < ApplicationRecord
+  PROPERTY_OPTIONS = ['pending', 'accepted', 'denied']
+
   belongs_to :kitchen
   belongs_to :user
   validates :start_time, :end_time, presence: true
+  validates :statut, inclusion: { in: [ true, false ] }
+  validates_inclusion_of :accepted, :in =>  PROPERTY_OPTIONS
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_100122) do
+ActiveRecord::Schema.define(version: 2020_02_20_183423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_02_19_100122) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "number_of_people"
+    t.boolean "statut", default: false
+    t.string "accepted", default: "pending"
     t.index ["kitchen_id"], name: "index_bookings_on_kitchen_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
